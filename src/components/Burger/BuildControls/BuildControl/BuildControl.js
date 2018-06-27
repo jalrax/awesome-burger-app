b25b6a8a5dceb7bb322f74eb724529c926f7364c
jalrax <trymental@gmail.com>
2018-06-27 14:11:50 +0300

    Purchase button added. Basic burger version is completed

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 5px 0;
+`;
+
+const Label = styled.div`
+    padding: 10px;
+    font-weight: bold;
+    width: 80px;
+`;
+
+const Button = styled.button`
+    display: block;
+    font: inherit;
+    padding: 5px;
+    margin: 0 5px;
+    width: 80px;
+    border: 1px solid #aa6817;
+    cursor: pointer;
+    outline: none;
+    
+  &:disabled {
+    background-color: #AC9980;
+    border: 1px solid #7E7365;
+    color: #ccc;
+    cursor: default;
+  }
+  
+  &:hover:disabled {
+    background-color: #AC9980;
+    color: #ccc;
+    cursor: not-allowed;
+  }
+`;
+
+const Less = styled(Button)`
+    background-color: #D39952;
+    color: white;
+    
+  &:hover, &:active {
+    background-color: #DAA972;
+    color: white;
+  }
+`;
+
+const More = styled(Button)`
+    background-color: #8F5E1E;
+    color: white;
+    
+  &:hover, &:active {
+    background-color: #99703F;
+    color: white;
+  }
+`;
+
+const buildControl = (props) => (
+    <Wrapper>
+        <Label>{props.label}</Label>
+        <Less onClick={props.removed} disabled={props.disabled}>Less</Less>
+        <More onClick={props.added}>More</More>
+    </Wrapper>
+);
+
+export default buildControl;
---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import BuildControl from './BuildControl/BuildControl';
+
+const Wrapper = styled.div`
+    width: 100%;
+    background-color: #cf8f2e;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    box-shadow: 0 2px 1px #ccc;
+    margin: auto;
+    padding: 10px 0;
+`;
+
+const Button = styled.button`
+    background-color: #DAD735;
+    outline: none;
+    cursor: pointer;
+    border: 1px solid #966909;
+    color: #966909;
+    font-family: inherit;
+    font-size: 1.2em;
+    padding: 15px 30px;
+    box-shadow: 2px 2px 2px #966909;
+    
+  &:hover, &:active {
+    background-color: #A0DB41;
+    border: 1px solid #966909;
+    color: #966909;
+  }
+  
+  &:disabled {
+    background-color: #C7C6C6;
+    cursor: not-allowed;
+    border: 1px solid #ccc;
+    color: #888888;
+  }
+  
+  &:not(:disabled) {
+    animation: enable 0.3s linear;
+  }
+  
+  @keyframes enable {
+    0% {
+        transform: scale(1);
+    }
+    60% {
+        transform: scale(1.1);
+    }
+    100% {
+        transform: scale(1);
+    }
+  }
+`;
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+];
+
+const buildControls = (props) => (
+    <Wrapper>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl => (
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
+        ))}
+        <Button disabled={!props.purchasable}>ORDER NOW</Button>
+    </Wrapper>
+);
+
+export default buildControls;
